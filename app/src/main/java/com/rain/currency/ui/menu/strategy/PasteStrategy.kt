5d7aca6c3dcc74b7551733604b7bec2b2a098cd2
Quan Vu <nongdenchet8435848@gmail.com>
2018-02-26 19:34:04 +0700

    handle invalid number paste

--- app/src/main/java/com/rain/currency/ui/menu/strategy/PasteStrategy.kt
+++ app/src/main/java/com/rain/currency/ui/menu/strategy/PasteStrategy.kt
@@ -1,11 +1,18 @@
 package com.rain.currency.ui.menu.strategy
 
 import android.content.ClipboardManager
+import android.view.Gravity
 import android.widget.EditText
+import android.widget.Toast
+import com.rain.currency.R
 import com.rain.currency.ui.menu.MenuStrategy
 import com.rain.currency.ui.menu.MenuType
+import java.lang.Exception
+import java.text.NumberFormat
 
 class PasteStrategy(private val clipboardManager: ClipboardManager, private val editText: EditText) : MenuStrategy {
+    private val numberFormatter = NumberFormat.getNumberInstance()
+
     override fun menuType() = MenuType.PASTE
 
     override fun execute() {
@@ -21,8 +28,16 @@ class PasteStrategy(private val clipboardManager: ClipboardManager, private val 
 
         primaryClip.getItemAt(0).text?.let {
             if (it.isNotBlank()) {
-                editText.setText(it)
-                editText.setSelection(it.length)
+                try {
+                    numberFormatter.parse(it.toString())
+                    editText.setText(it)
+                    editText.setSelection(it.length)
+                } catch (e: Exception) {
+                    val context = editText.context
+                    val toast = Toast.makeText(context, context.getString(R.string.invalid_amount), Toast.LENGTH_LONG)
+                    toast.setGravity(Gravity.TOP, 0, 0)
+                    toast.show()
+                }
             }
         }
     }
